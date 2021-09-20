a99fb19c19f64423ce747ba5d58be4960f6c3f42
hahuaz <hahuaz@gmail.com>
2021-09-20 15:14:11 +0300

    add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
       lang: 'en',
     },
     meta: [
+      { name: 'my custom 1', content: 'my value 1' },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
